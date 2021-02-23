beac7bcff9d7c86c6fc4be34eae2e082765079cd
Francisco Beltrán <franciscobel@correo.ugr.es>
2021-02-23 13:27:08 +0100

    Añadiendo modelos al administrador de la BBDD

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Asignatura, Area, Mencion, Titulo, AñoAsignatura, Grupo
 
 # Register your models here.
+admin.site.register(Asignatura)
+admin.site.register(Area)
+admin.site.register(Mencion)
+admin.site.register(Titulo)
+admin.site.register(AñoAsignatura)
+admin.site.register(Grupo)
