9f15d0831b5cefeb19569ceb5e26091fd6e42d2c
Francisco Beltrán <franciscobel@correo.ugr.es>
2021-02-25 11:27:20 +0100

    Añadiendo diseño buscador base de datos

--- src/app/views.py
+++ src/app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'index.html', {})
+
+def buscadorBBDD(request):
+    return render(request, 'buscador.html', {})
