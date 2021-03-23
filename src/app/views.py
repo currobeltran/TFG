f2e68fb8e03e637e6b7c3e9fd52c1627dd64fa36
Francisco Beltrán <franciscobel@correo.ugr.es>
2021-03-23 19:04:31 +0100

    Añadida documentacion vistas

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,5 @@
+# En este documento se almacenarán las vistas de la aplicación web
+
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import ObtenerRegistros
@@ -7,10 +9,28 @@ import plotly.offline as plotly
 import numpy as np
 import plotly.graph_objs as go
 
+# Inicio: Vista inicial de la aplicación
+# TODO: Añadir un parámetro al renderizado de index.html para que se pueda
+# personalizar el mensaje que aparece
 def inicio(request):
     registrado = estaRegistrado(request)
     return render(request, 'index.html', {'registrado':registrado})
 
+# BuscadorBBDD: Vista que nos permitirá realizar la operación de búsqueda y
+# visualización de información en la base de datos con el objetivo de obtener 
+# distintas conclusiones acerca de los datos almacenados.
+# 
+# La información se podrá mostrar de 2 maneras distintas: en forma de tabla
+# y en forma de gráfica, mientras que la información que se pretende mostrar
+# será la correspondiente a los grupos de uno o varios años académicos seleccionados
+# para una o varias asignaturas concretas. Además, la información de los grupos que 
+# se desea visualizar en la tabla o gráfica puede ser cambiada, para ver solo los 
+# datos que nos interesen.
+# 
+# TODO:
+#   - Introducir el modo gráfica.
+#   - Especificar mejor las características de la visualización de la información que
+#     se desea.
 def buscadorBBDD(request):
     registrado = estaRegistrado(request)
     if request.method == "GET":
@@ -62,6 +82,13 @@ def buscadorBBDD(request):
 
         return render(request, 'resultadobusqueda.html', {'registrado':registrado,'graph':graph})
 
+# EditarBBDD: Vista inicial de la función de edición.
+# 
+# Dicha función tendrá la capacidad tanto de añadir, eliminar y editar 
+# registros presentes en la Base de Datos, pero en esta primera vista
+# se mostrará al usuario si desea añadir información de manera masiva
+# o de manera individualizada (donde se podrá tanto añadir como editar
+# y eliminar).
 def editarBBDD(request):
     registrado = estaRegistrado(request)
     if not registrado:
@@ -70,6 +97,10 @@ def editarBBDD(request):
 
     return render(request, 'editor.html', {'registrado':registrado})
 
+# InfomasivaBBDD: Vista para la introducción de información masiva en la 
+# Base de Datos.
+# 
+# TODO: Dar funcionalidad, solo está diseñada la interfaz.
 def infomasivaBBDD(request):
     registrado = estaRegistrado(request)
     if not registrado:
@@ -78,6 +109,20 @@ def infomasivaBBDD(request):
 
     return render(request, 'infomasiva.html', {'registrado':registrado})
 
+# Editaindividual: Vista para la edición de información individual.
+# 
+# En esta función se mostrarán 2 menús desplegables con el tipo de dato
+# y los objetos del tipo de dato seleccionado respectivamente. Aparecerán 2 
+# botones, uno para generar el formulario de edición y otro para la función de 
+# eliminación. 
+# 
+# Dentro del menú desplegable que contendrá los distintos elementos 
+# almacenados en la Base de Datos correspondientes a un tipo de dato concreto, siempre
+# existirá la opción de crear un elemento nuevo; con el que nos aparecerá un formulario
+# en blanco para crear el registro que deseamos. Si en cambio, seleccionasemos un objeto
+# ya existente, se generaría el mismo formulario pero relleno con la información del 
+# registro seleccionado. Si en vez de editarlo queremos eliminarlo, aparecerá el mismo 
+# formulario de manera que no se puedan editar ninguno de los campos que posea.
 def editaindividual(request):
     registrado = estaRegistrado(request)
     if not registrado:
@@ -86,6 +131,14 @@ def editaindividual(request):
 
     return render(request, 'infoindividual.html', {'registrado':registrado})
 
+# ApibuscaBBDD: Vista para realizar la función de aparición dinámica de 
+# los registros de un tipo en concreto.
+#
+# Básicamente esta función nos ayudará a que, cuando cambiemos de tipo de 
+# dato en el menú desplegable de edición de información, se muestren en el
+# menú de registros los correspondientes a dicho tipo de dato. En la función se realiza 
+# la búsqueda de esta información en la Base de Datos, y el resultado obtenido se envía
+# al código JavaScript que ha solicitado esta información a través de una petición AJAX.
 def apibuscaBBDD(request):
     x = ObtenerRegistros(request.GET.get('tipo'))
     data = {}
@@ -94,6 +147,9 @@ def apibuscaBBDD(request):
 
     return JsonResponse(data)
 
+# CopiaSeguridad: Vista para realizar una copia de seguridad de la información almacenada
+# en la Base de Datos.
+# TODO: Implementar funcionalidad, solo está diseñada la interfaz.
 def copiaSeguridad(request):
     registrado = estaRegistrado(request)
     if not registrado:
@@ -102,6 +158,12 @@ def copiaSeguridad(request):
 
     return render(request, 'copiaseguridad.html', {'registrado':registrado})
 
+# EditaUsuario: Vista para que el usuario que esté registrado en ese momento en la aplicación
+# pueda editar su infomación personal.
+# 
+# TODO: Cuando se realiza un cambio efectivo en la información del usuario, se debe dar más
+# información a este para que sepa que debe iniciar sesión de nuevo en la aplicación (o implementar
+# el inicio de sesión automático tras cambios).
 def editaUsuario(request):
     registrado = estaRegistrado(request)
     if not registrado:
@@ -119,6 +181,18 @@ def editaUsuario(request):
         else:
             return render(request, 'editausuario.html', {'registrado':registrado, 'usuario':request.user, 'form':form})
 
+# FormularioEdicion: Vista encargada de realizar las acciones cuando se genera un 
+# formulario de edición individual en la vista Editaindividual. 
+#
+# Si la petición que le llega a esta vista es de tipo GET, se genera el formulario
+# dependiendo de las características que se hayan introducido anteriormente (se va
+# a crear un nuevo elemento->Formulario vacío; se va a editar un elemento ya
+# existente->Formulario relleno; se va a eliminar un elemento->Formulario relleno
+# no editable).
+#
+# Si la petición que llega a la función en cambio es de tipo POST, se procesa la información
+# que se haya introducido en el formulario para realizar la acción correspondiente (crear,
+# editar) y guardarla en la Base de Datos.
 def formularioEdicion(request):
     registrado = estaRegistrado(request)
     form = ''
@@ -362,6 +436,8 @@ def formularioEdicion(request):
     return render(request, 'formulariogenerado.html', {'registrado':registrado,'form':form,'nuevo':nuevo,'tipo':request.GET.get('selecciontipo'),
     'objeto':request.GET.get('seleccionobjeto')})
 
+# EliminaRegistro: Vista para realizar la eliminación de un registro almacenado
+# en la Base de Datos.
 def eliminaRegistro(request):
     registrado = estaRegistrado(request)
     # Obtener elemento a eliminar a traves de id (desde url) y mandar peticion de eliminado a BBDD
